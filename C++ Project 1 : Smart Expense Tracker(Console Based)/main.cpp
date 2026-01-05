1837b693142225946a6982ba944d1ba6830219b0
shirani11 <shiranigt@gmail.com>
2026-01-06 01:49:11 +0900

    Initial Commit

---
+++ C++ Project 1 : Smart Expense Tracker(Console Based)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++ Project 1 : Smart Expense Tracker(Console Based)
+//
+//  Created by shirani on 06/01/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
